4ec67de7877f318ffe0c99dcffdfca89d0ae3a6c
aisha12 <smdimran838@gmial.com>
2019-02-26 22:54:11 +0530

    test

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask , render_template , redirect ,  url_for , flash , session
+
+
+app = Flask(__name__)
+
+
+@app.route('/',methods = ['GET'])
+def intro() :
+    return render_template('base.html')    
+
+
+if __name__ == '__main__':
+    from os import environ
+    app.run(port=environ.get("PORT", 5000), debug = True)
